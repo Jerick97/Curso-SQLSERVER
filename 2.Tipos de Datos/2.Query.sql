28736b2815d07979eab03c1255170a79d3f8c8aa
Emerson Suarez <emersonsuarez2904@gmail.com>
2024-08-09 15:19:51 -0500

    feat: :memo: Agrega Tipos de Datos

---
+++ 2.Tipos de Datos/2.Query.sql
@@ -0,0 +1,16 @@
+-- Tipos de Datos
+
+CHAR --Almacena tipo de datos de ancho fijo alfanumericos.
+VARCHAR --Almacena tipo de datos alfanumericos de ancho variable.
+TEXT --Almacena tipos de datos texto.
+NCHAR --Almacena tipos de datos de ancho fijo, elimina el espacio sobrante.
+NVARCHAR --Almacena tipos de datos alfanumericos de ancho variable, elimina el espacio sobrante.
+BIT --Almacena valores de 1 y 0.
+INT --Almacena valores entre -2,147,483,648 y 2,147,483,647.
+BIGINT --Almacena valores entre -9,223,372,036,854,775,808 y 9,223,372,036,854,775,807.
+DECIMAL --Almacena valores decimales entre -10^38 + 1 to 10^38-1.
+NUMERIC --Almacena valores enteros entre -10^38 + 1 to 10^38-1.
+MONEY --Almacena valores entre -9,223,372,036,854,775,808 y 9,223,372,036,854,775,807.
+FLOAT --Almacena valores entre -1.79E + 308 to 1.79E + 308.
+ 
+
